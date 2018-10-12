9c49cfd7eb805aaad65ddc86cdfc4f83840361a4
JulienS13 <sorelli.julien@gmail.com>
2018-10-12 18:07:05 +0200

    mustache /

--- src/app.js
+++ src/app.js
@@ -6,7 +6,21 @@ const _templateDir = 'src/statics';
 const fs = require('fs');
 const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', function (req, res) {
+    
+    let filePath = path.join(_templateDir, '/accueil.html');
+
+    fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+        if (!err) {
+            console.log('request ', );
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        } else {
+            console.log(err);
+        }
+    });
+});
 
 app.get('/about', function (req, res) {
     let filePath = path.join(_templateDir, '/propos.html');
