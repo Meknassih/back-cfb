a79049615c7984ac37183c92c10350447949b5d3
bennani <soufiane.bennani@ynov.com>
2018-10-12 17:22:37 +0200

    Ajout des routes (Home,About,Word,Login,Logout)

--- src/app.js
+++ src/app.js
@@ -3,6 +3,29 @@ const mustache = require('mustache');
 const app = express();
 const port = 3000;
 
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/about', function(req, res){
+    res.send('About page !')
+});
+
+app.get('/word', function(req, res){
+    res.send('Word page')
+});
+
+app.get('/login', function(req, res){
+    res.send('Login page')
+});
+
+app.get('/logout', function(req, res){
+    res.send('Logout page')
+
+});
+
+app.get('*', function (req,res) {
+    res.redirect('/');
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
