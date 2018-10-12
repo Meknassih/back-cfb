beeaaeb738c9082c52ef33b64369bbe725bdf2c6
Hamza Meknassi <elmeknassi.h@gmail.com>
2018-10-12 17:51:28 +0200

    /login read template and outputs it

--- src/app.js
+++ src/app.js
@@ -2,28 +2,41 @@ const express = require('express');
 const mustache = require('mustache');
 const app = express();
 const port = 3000;
-
+const _templateDir = 'src/statics';
+const fs = require('fs');
+const path = require('path');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/about', function(req, res){
+app.get('/about', function (req, res) {
     res.send('About page !')
 });
 
-app.get('/word', function(req, res){
+app.get('/word', function (req, res) {
     res.send('Word page')
 });
 
-app.get('/login', function(req, res){
-    res.send('Login page')
+app.get('/login', function (req, res) {
+    let filePath = path.join(_templateDir, '/identification.html');
+
+    fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+        if (!err) {
+            console.log('request ', );
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        } else {
+            console.log(err);
+        }
+    });
 });
 
-app.get('/logout', function(req, res){
+app.get('/logout', function (req, res) {
     res.send('Logout page')
 
 });
 
-app.get('*', function (req,res) {
+app.get('*', function (req, res) {
     res.redirect('/');
 });
 
