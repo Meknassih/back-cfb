b243204803fbb89f50124ea5bccea7ad9dc6312a
bennani <soufiane.bennani@ynov.com>
2018-10-18 17:34:32 +0200

    Création du model 'User' et génération du token JWT

---
+++ src/Models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    username: String,
+    password: String,
+    email: String,
+    salt: String,
+    avatarUrl: String,
+    isOnline: Boolean,
+    lastSeen: Date
+    }, {
+        timestamps: true,
+    });
+
+
+UserSchema.methods.generateJWT = function() {
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        username: this.username,
+        id: this._id,
+        exp: parseInt(expirationDate.getTime() / 1000, 10),
+    }, 'secret');
+};
+
+
+UserSchema.methods.toAuthJSON = function() {
+    return {
+        _id: this._id,
+        username: this.username,
+        email: this.email,
+        avatarUrl: this.avatarUrl,
+        lastSeen: this.lastSeen,
+        token: this.generateJWT(),
+    };
+};
+
+mongoose.model('User', UserSchema);
